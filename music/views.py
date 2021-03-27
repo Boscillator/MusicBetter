75eda4d15c2887b02872e64b458f3067c0ef44a4
WMonahan <william.monahan@duke.edu>
2021-03-27 18:19:32 -0400

    Added view for form

--- music/views.py
+++ music/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
 
+from .forms import PieceForm
 
 def index(request):
-    template = loader.get_template('music/index.html')
-    context = {}
-    return HttpResponse(template.render(context, request))
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = PieceForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/results/')
+
+    else:
+        template = loader.get_template('music/index.html')
+        context = {}
+        return HttpResponse(template.render(context, request))
 
 def results(request):
     template = loader.get_template('music/submit.html')
