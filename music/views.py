c2ebf5013b97364b508f8e6633fb140b1880651b
RoboticPanda77 <natmonahan77@gmail.com>
2021-03-27 16:02:25 -0400

    Preliminary HTML

--- music/views.py
+++ music/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the music index.")
+    template = loader.get_template('music/index.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
