edf8dea2e078ac541557b79e62ab32d3409cb75e
Fred Buchanan <oscillator.b@gmail.com>
2021-03-27 23:02:21 -0400

    Added normalization

--- music/admin.py
+++ music/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib import admin
 
-from .models import Song
+from .models import Song, Comp
 
 admin.site.register(Song)
+admin.site.register(Comp)
